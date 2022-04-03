9b63f2fb91a210629b965c2b0e051c53b67a982e
ercheung3 <ercheung3@gmail.com>
2022-04-03 08:56:25 -0700

    Updated item model.

---
+++ models/item.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema(
+  {
+    productName: { type: String, unique: true, required: true },
+    quantity: { type: Number, default: 0, required: true },
+  },
+  { timestamps: true }
+);
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
