44d5326458d2d5971f00ee8dc1232da21c5d4a27
ercheung3 <ercheung3@gmail.com>
2022-04-03 09:24:47 -0700

    Redirect from songs to root.

--- app.js
+++ app.js
@@ -39,9 +39,14 @@ app.use(
   })
 );
 
+app.get("/", (req, res) => {
+  res.redirect("/songs");
+});
+
 app.use("/songs", songController);
 
 const port = process.env.PORT || 3001;
+
 app.listen(port, () => {
   console.log("App Running: " + port);
 });
